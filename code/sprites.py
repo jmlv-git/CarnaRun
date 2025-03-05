5c54efac252c8cd20cbd07f06d101c0175e24b2a
gso <gso@cin.ufpe.br>
2025-03-05 19:37:12 -0300

    basic logic

---
+++ code/sprites.py
@@ -0,0 +1,15 @@
+from settings import * 
+from math import atan2, degrees
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+        self.ground = True
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
